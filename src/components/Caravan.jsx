1893ec732c6a4ac92195d5a30360dcc282789be7
dkobrin <shdwkat999@yahoo.com>
2023-03-16 00:22:10 -0400

    almost playable as pvp local

--- src/components/Caravan.jsx
+++ src/components/Caravan.jsx
@@ -35,34 +35,34 @@ const Caravan = (props) => {
     }
 
     const dragDropCaravan = (e, data) => {
-    // e.preventDefault();
-    const tmpDragData = {...data};
-    console.log({dragData}, {data}, 'in dragDropCaravan');
-    if((gameData.isPlayer1Turn && caravan.owner === 'player1') || (!gameData.isPlayer1Turn && caravan.owner === 'player2') ) {
-        // move dragged item
-        // add item to new location
-        const tmpGameData = {...gameData};
-        let dropLoc = tmpGameData;
-        tmpDragData.dragTarget.forEach(el => {
-            dropLoc = dropLoc[el];
-        });
-        let srcLoc = tmpGameData;
-        tmpDragData.dragItem.srcLoc.forEach(el => {
-            srcLoc = srcLoc[el];
-        });
-        dropLoc.push([srcLoc[tmpDragData.dragItem.idx]]);
-        // remove item from old location
-        srcLoc.splice(tmpDragData.dragItem.idx, 1);
-        setGameData(tmpGameData);
-        // console.log( JSON.stringify(gameData), 'before handleEndMove');
-        gameData.handleEndMove(tmpGameData);
-        // console.log({gameData}, 'after handleEndMove');
-    }
-    // clear dragging info as player is not dragging anymore
-    tmpDragData.dragTarget = null;
-    tmpDragData.dragItem = null;
+        // e.preventDefault();
+        const tmpDragData = {...data};
+        console.log({dragData}, {data}, 'in dragDropCaravan');
+        if((gameData.isPlayer1Turn && caravan.owner === 'player1') || (!gameData.isPlayer1Turn && caravan.owner === 'player2') ) {
+            // move dragged item
+            // add item to new location
+            const tmpGameData = {...gameData};
+            let dropLoc = tmpGameData;
+            tmpDragData.dragTarget.forEach(el => {
+                dropLoc = dropLoc[el];
+            });
+            let srcLoc = tmpGameData;
+            tmpDragData.dragItem.srcLoc.forEach(el => {
+                srcLoc = srcLoc[el];
+            });
+            dropLoc.push([srcLoc[tmpDragData.dragItem.idx]]);
+            // remove item from old location
+            srcLoc.splice(tmpDragData.dragItem.idx, 1);
+            setGameData(tmpGameData);
+            // console.log( JSON.stringify(gameData), 'before handleEndMove');
+            gameData.handleEndMove(tmpGameData);
+            // console.log({gameData}, 'after handleEndMove');
+        }
+        // clear dragging info as player is not dragging anymore
+        tmpDragData.dragTarget = null;
+        tmpDragData.dragItem = null;
 
-    setDragData(tmpDragData);
+        setDragData(tmpDragData);
     }
 
     const dragStart = (e, idx) => {
